64988caa2461a22df9012373f79820c3936d8451
Owen Ganza <o.ganza@alu.com>
2025-05-27 21:55:11 +0200

    commit to clone

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,7 +8,13 @@ sudo apt-get -y install nginx
 # create folders
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 sudo touch /data/web_static/releases/test/index.html
-sudo echo "This is a sample text" | sudo tee /data/web_static/releases/test/index.html
+sudo echo "<html>
+<head>
+ </head>
+<body>
+  Holberton School
+</body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
