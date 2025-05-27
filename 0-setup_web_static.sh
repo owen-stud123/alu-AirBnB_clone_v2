9772bd920df5d70c4df2e315b5961faf7c7ef7ac
Owen Ganza <o.ganza@alu.com>
2025-05-27 21:44:48 +0200

    commit to clone

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,13 +8,7 @@ sudo apt-get -y install nginx
 # create folders
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 sudo touch /data/web_static/releases/test/index.html
-sudo echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+sudo echo "This is a sample text" | sudo tee /data/web_static/releases/test/index.html
 
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
@@ -23,3 +17,4 @@ sudo sed -i "/listen 80 default_server/a \\
 \\n\\tlocation /hbnb_static/ {\\n\\t\\talias /data/web_static/current/;\\n\\t}\\n" /etc/nginx/sites-available/default
 
 sudo service nginx restart
+
