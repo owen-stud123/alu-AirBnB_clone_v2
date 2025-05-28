3738230e95e8163c56f15d6e28cd6295277f8bd5
Owen Ganza <o.ganza@alu.com>
2025-05-29 01:11:40 +0200

    commit

--- models/state.py
+++ models/state.py
@@ -2,28 +2,27 @@
 """ State Module for HBNB project """
 import models
 from models.base_model import BaseModel, Base
-from models.city import City
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
+import os
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-      __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state')
 
-     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship('City', backref="state"
-                               cascade="all, delete, delete-orphan")
+    __tablename__ = "states"
+
+    name = Column(String(128), nullable=False)
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref="state",
+                              cascade="all, delete, delete-orphan")
     else:
         @property
-         def cities(self):
-                 """Get a list of all related City objects."""
-            city_list = []
-            for city in list(storage.all(City).values()):
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+        def cities(self):
+            """ Returns the list of City instances with state_id
+            equals to the current State.id. """
+            return [city for city in models.storage.all(City).values()
+                    if city.state_id == self.id]
+
