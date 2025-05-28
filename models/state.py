7dbfafd941b589a8b65fee5994161f3e82ae9ede
Owen Ganza <o.ganza@alu.com>
2025-05-28 02:45:09 +0200

    commit all

--- models/state.py
+++ models/state.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+from models.city import City
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+      __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state')
+
+     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref="state"
+                               cascade="all, delete, delete-orphan")
+    else:
+        @property
+         def cities(self):
+                 """Get a list of all related City objects."""
+            city_list = []
+            for city in list(storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
